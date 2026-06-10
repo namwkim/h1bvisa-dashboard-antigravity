54ed6e7c368cfeed14886f6292545bb30f5a2827
Nam Wook Kim <namwookkim@Nams-MacBook-Air.local>
2026-06-10 14:46:34 -0400

    Configure project for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/h1bvisa-dashboard-antigravity/",
   server: {
     port: 8080,
     strictPort: true
